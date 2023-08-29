6c0f45847211d78b4f43e2b42060dd6bcc2ea1b3
Micah Njeru <micahnjeru1@gmail.com>
2023-08-29 19:06:51 +0300

    Updating Variables swift file

--- Course-materials/Variables.playground/Contents.swift
+++ Course-materials/Variables.playground/Contents.swift
@@ -1,3 +1,22 @@
 import Foundation
 
+// There are two ways of storing variables
+// Using the "let" keyword and "var" keyword
+/// The only thing different between these two is that,
+/// the "let" variable cannot be assigned to again whereas
+/// the "var" variable can be assigned to again.
+///
 
+// Constants
+let myName = "Micah"
+let yourName = "Vandad"
+
+// Variables
+var names = [
+    myName,
+    yourName
+]
+
+// Adding to the array
+names.append("Bar")
+names.append("Foo")
