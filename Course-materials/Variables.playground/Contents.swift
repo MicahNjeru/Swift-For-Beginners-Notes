2d6f457d48b5cd0d2773941725e6fd51d28ddfd8
Micah Njeru <micahnjeru1@gmail.com>
2023-08-29 19:33:31 +0300

    Final edit of the variable swift file

--- Course-materials/Variables.playground/Contents.swift
+++ Course-materials/Variables.playground/Contents.swift
@@ -3,8 +3,8 @@ import Foundation
 // There are two ways of storing variables
 // Using the "let" keyword and "var" keyword
 /// The only thing different between these two is that,
-/// the "let" variable cannot be assigned to again whereas
-/// the "var" variable can be assigned to again.
+/// the "let" variable cannot be assigned to again (immutable) whereas
+/// the "var" variable can be assigned to again (mutable).
 ///
 
 // Constants
