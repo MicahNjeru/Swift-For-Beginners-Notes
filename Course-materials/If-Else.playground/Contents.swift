f8a6d7d788badf9910433db53d3a9630861751b6
Micah Njeru <micahnjeru1@gmail.com>
2023-09-03 12:52:44 +0300

    Adding If-Else playground

--- Course-materials/If-Else.playground/Contents.swift
+++ Course-materials/If-Else.playground/Contents.swift
@@ -1,3 +1,23 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+
+let myName = "Micah"
+let myAge = 25
+let yourName = "Vandad"
+let yourAge = 20
+
+if myName == "micah" {
+    "Your name is \(myName)"
+} else {
+    "Opps, I guessed it wrong"
+}
+
+if myName == "Micah" {
+    "Now I guessed it correctly"
+} else if myName == "Foo" {
+    "Are you Foo?"
+} else {
+    "Okay, I give up"
+}
+
+
