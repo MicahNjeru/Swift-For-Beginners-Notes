922327a3441f882790bdb861a74459de48e83258
Micah Njeru <micahnjeru1@gmail.com>
2023-09-03 13:06:45 +0300

    Synchronizing If-Else playground

--- Course-materials/If-Else.playground/Contents.swift
+++ Course-materials/If-Else.playground/Contents.swift
@@ -6,18 +6,36 @@ let myAge = 25
 let yourName = "Vandad"
 let yourAge = 20
 
+// If-Else
 if myName == "micah" {
     "Your name is \(myName)"
 } else {
     "Opps, I guessed it wrong"
 }
 
+// If-Else-Else if
 if myName == "Micah" {
     "Now I guessed it correctly"
 } else if myName == "Foo" {
     "Are you Foo?"
 } else {
     "Okay, I give up"
+}
+
+// Compounded "and" If-Else-Else if conditional
+if myName == "Micah" && myAge == 25{
+    "Name is Micah & age is 25"
+} else if myAge == 25 {
+    "I only guessed the age right"
+} else {
+    "I don't know what I'm doing"
+}
+
+// Compounded "or" If-Else if conditional
+if myAge == 25 || myName == "Foo" {
+    "Either age is 20, name is Foo or both"
+} else if myAge == 30 || myName == "Micah" {
+    "It's too late to get in this clause"
 }
 
 
