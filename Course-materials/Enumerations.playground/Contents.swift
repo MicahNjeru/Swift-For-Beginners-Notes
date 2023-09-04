be8e8ff4e53562ac027ba169b2dcced9905179e4
Micah Njeru <micahnjeru1@gmail.com>
2023-09-04 18:46:15 +0300

    Added code and explanations

--- Course-materials/Enumerations.playground/Contents.swift
+++ Course-materials/Enumerations.playground/Contents.swift
@@ -1,3 +1,33 @@
 import Foundation
 
+/// Enumerations are: categorization of similar values that are named. Such as values.
+
+enum Animals {
+    case cat
+    case dog
+    case rabbit
+}
+
+let cat = Animals.cat
+cat
+
+if cat == Animals.cat {
+    "This is a cat"
+} else if cat == Animals.dog {
+    "This is a dog"
+} else if cat == Animals.rabbit {
+    "Definitely something else"
+}
+
+// Using switch statements
+// Switch statements must be exhaustively covered
+switch cat {
+case .cat:
+    "This is a cat"
+case .dog:
+    "This is a dog"
+case .rabbit:
+    "This is a rabbit"
+}
+
 
