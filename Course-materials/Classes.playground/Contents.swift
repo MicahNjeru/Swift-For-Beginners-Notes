1c9c1ad37eaf7a1f9deb80990352705766874b1c
Micah Njeru <micahnjeru1@gmail.com>
2023-09-05 10:24:19 +0300

    Added more code and examples

--- Course-materials/Classes.playground/Contents.swift
+++ Course-materials/Classes.playground/Contents.swift
@@ -34,9 +34,81 @@ let bar = foo
 bar.increaseAge()
 bar.age
 foo.age
+foo.age += 10
 
 if foo === bar {
     "Foo and bar point to the same memory"
 } else {
     "They don't point to the same memory"
 }
+
+// Sub-classing
+class Vehicle {
+    func goVroom() {
+        "Vroom vroom"
+    }
+}
+
+class Car: Vehicle {
+    
+}
+
+let car = Car()
+car.goVroom()
+
+
+// Setting internal class variable as internal mutable only
+class Human {
+    private(set) var age: Int
+    
+    init(age: Int){
+        self.age = age
+    }
+    
+    func increaseAge() {
+        self.age += 1
+    }
+}
+
+let baz = Human(age: 20)
+bar.age
+bar.increaseAge()
+
+
+/// Designated Initializers: construct an instance of the class by ensuring all property values & structure of the class
+/// holds up.
+/// Convenience initializers: add some logic & delegate initialization to the designated initializers
+
+// Examples:
+class Tesla {
+    let manufacturer = "Tesla"
+    let model: String
+    let year: Int
+    
+    // Designated initilizer
+    init() {
+        self.model = "X"
+        self.year = 2023
+    }
+    
+    // Designated initilizer
+    init(model: String, year: Int){
+        self.model = model
+        self.year = year
+    }
+    
+    // Convenience initializer
+    convenience init (model: String){
+        self.init(model: model, year: 2023)
+    }
+}
+
+// Sub-classing Tesla
+// The convenience initializer cannot be called from the super class initializer
+class TeslaModelY: Tesla {
+    override init() {
+        super.init(model: "Y", year: 2023)
+    }
+}
+
+
