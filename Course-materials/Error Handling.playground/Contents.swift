08ad94f6e312187fe819fec44520393936e08bbe
Micah Njeru <micahnjeru1@gmail.com>
2023-09-06 17:11:48 +0300

    Added basic error throwing, catching and handling code with examples

--- Course-materials/Error Handling.playground/Contents.swift
+++ Course-materials/Error Handling.playground/Contents.swift
@@ -1,3 +1,62 @@
 import Foundation
 
-/// Error Handling: 
+/// Error Handling: The science & art of telling something has gone wrong and how to handle it.
+
+struct Person {
+    let firstName: String?
+    let lastName: String?
+    
+    enum Errors: Error {
+        case firstNameIsNil
+        case lastNameIsNil
+        case bothNamesAreNil
+    }
+    
+    func getFullName() throws -> String {
+        switch (firstName, lastName) {
+        case (.none, .none):
+            throw Errors.bothNamesAreNil
+        case (.none, .some):
+            throw Errors.firstNameIsNil
+        case (.some, .none):
+            throw Errors.lastNameIsNil
+        case let (.some(firstName), .some(lastName)):
+            return "\(firstName) \(lastName)"
+        }
+    }
+}
+
+let foo = Person(firstName: "Foo", lastName: nil)
+
+// When using throw statements, we need to use do-try to "catch" the "thrown" errors
+do {
+    let fullName = try foo.getFullName()
+} catch {
+    "Got an error \(error)"
+}
+
+// Using specific errors from the struct
+do {
+    let fullName = try foo.getFullName()
+    fullName
+} catch is Person.Errors {
+    "Got an error"
+}
+
+// How to catch specific errors without know which error it is
+let bar = Person(firstName: nil, lastName: nil)
+
+do {
+    let fullName = bar.getFullName()
+    fullName
+} catch Person.Errors.firstNameIsNil {
+    "First name is nil"
+} catch Person.Errors.lastNameIsNil {
+    "Last name is nil"
+} catch Person.Errors.bothNamesAreNil {
+    "Both name are nill"
+} catch {
+    "Some other error was thrown"
+}
+
+
